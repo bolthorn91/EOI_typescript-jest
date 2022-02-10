a6e1728b80ef55ab300506ed5ef146792f01214a
bolthorn91 <bolthorn91@gmail.com>
2022-02-10 19:17:50 +0000

    Big bang

---
+++ src/exclamator.ts
@@ -0,0 +1,1 @@
+// TODO Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner, you can assume that the input data is always a non empty string, no need to verify it.
---
+++ src/math-operators.ts
@@ -0,0 +1,19 @@
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+function sub(a: number, b: number): number {
+    return a - b;
+}
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function div(a: number, b: number): number {
+    return a / b;
+}
+
+// To Implement: sumAll, subAll, multiplyAll, divAll
+
+export { sum, sub, multiply, div };
